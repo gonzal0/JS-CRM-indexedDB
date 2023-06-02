697f9166c63cf8e396c451b82351e2502b32560e
gonzal0 <huyke12@gmail.com>
2023-06-01 21:22:12 -0500

    se agrego

---
+++ js/nuevocliente.js
@@ -0,0 +1,56 @@
+(function () {
+  
+  const formulario = document.querySelector('#formulario');
+
+  document.addEventListener('DOMContentLoaded', () => {
+    conectarDB();
+    formulario.addEventListener('submit', validarCliente);
+  });
+
+  function validarCliente(e) {
+    e.preventDefault();
+
+    //Leer todos los inputs
+    const nombre = document.querySelector('#nombre').value;
+    const email = document.querySelector('#email').value;
+    const telefono = document.querySelector('#telefono').value;
+    const empresa = document.querySelector('#empresa').value;
+
+    if (nombre === '' || email === '' || telefono === '' || empresa === '') {
+      imprimirAlerta('Todos los campos son obligatorios', 'error');
+      return;
+    }
+
+    //crear un objeto con la imformacion
+    const cliente = {
+      nombre,
+      email,
+      telefono,
+      empresa,
+    };
+
+    cliente.id = Date.now();
+    crearNuevoCliente(cliente);
+  }
+
+  function crearNuevoCliente(cliente) {
+
+    const transaction = DB.transaction(['crm'],'readwrite')
+    const objectStore = transaction.objectStore('crm');
+    objectStore.add(cliente);
+
+    transaction.onerror = function(){
+      imprimirAlerta('hubo un error');
+    }
+    transaction.oncomplete = function(){
+    //  console.log('cliente agregado');
+      imprimirAlerta('cliente agregado correctamente');
+      
+      setTimeout(() => {
+        window.location.href= 'index.html';
+      },3000);
+    }
+
+  }
+
+})();
